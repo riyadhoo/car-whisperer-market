41f57cae7f952f450c67e95719b11d52cf11f3c3
riyadh maameri <129912677+riyadhoo@users.noreply.github.com>
2025-05-09 19:36:11 +0100

    zd

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -20,7 +20,7 @@ const Navigation = () => {
               <img 
                 src="/public/lovable-uploads/69914926-946e-47e7-b0e6-fe2b91025fc1.png" 
                 alt="Torque Up" 
-                className="w-16 h-10"
+                className="w-17 h-10"
               />
             </div>
           </Link>
